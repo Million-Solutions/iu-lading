25c2e34dbdde350ae4154dd5755727cd91e48a98
Angel <zaggel2002@gmail.com>
2022-10-12 21:13:10 -0500

    Design fixes

--- src/app/pages/home-landing/home-landing.component.ts
+++ src/app/pages/home-landing/home-landing.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-landing.component.scss']
 })
 export class HomeLandingComponent implements OnInit {
+  str = 'Mensual'
+  letsTalk = true
+  array = ['$35.000','$65.000','$90.000']
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeMonth(str: string) {
+    if(str === 'Mensual') {
+      this.str = str
+      this.array = ['$35.000','$65.000','$90.000']
+    } else if (str === 'Semestral') {
+      this.str = str
+      this.array = ['$200.000','$370.000','$510.000']
+    } else {
+      this.str = str
+      this.array = ['$390.000','$725.000','$1.005.000']
+    }
   }
 
 }
