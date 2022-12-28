9290d6e5c23de302b6bf4d5aa49c77eb7f77f64f
Angel <zaggel2002@gmail.com>
2022-12-28 11:43:33 -0500

    added video

--- src/app/pages/home-landing/home-landing.component.ts
+++ src/app/pages/home-landing/home-landing.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-declare var bootstrap:any
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-landing',
   templateUrl: './home-landing.component.html',
   styleUrls: ['./home-landing.component.scss']
 })
-export class HomeLandingComponent implements OnInit {
-  str = 'Mensual'
-  price: Array<string> = ['$35.000','$65.000','$90.000']
-  form: FormGroup
-  showModal:boolean = false
-  myModal: any
 
+export class HomeLandingComponent implements OnInit {
+  price: Array<string> = ['$35.000','$65.000','$90.000']
+  alert: any = {type: '', text: ''}
+  spinner: boolean = false
+  form: FormGroup
+  
   constructor() {}
 
   ngOnInit(): void {
     this.initForm()
   }
 
-  initForm(){
+  initForm() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
       email: new FormControl('', Validators.required),
@@ -29,24 +28,21 @@ export class HomeLandingComponent implements OnInit {
       count_client: new FormControl('1', Validators.required),
     })
   }
-
-  sendData(){
-    if(this.form.invalid) return 
-  }
-
-  b(){
-    this.showModal = true
+  
+  sendForm() {
+    if(this.form.invalid) return this.Alert('danger', 'Asegurate de haber completado todos los campos!')
+    this.spinner = true
     setTimeout(() => {
-      const a:any = document.getElementById('modalCalendly')
-      this.myModal = new bootstrap.Modal(a, {})
-      a.addEventListener('hidden.bs.modal', () =>{
-        this.showModal = false
-      })
-      this.myModal.show()
-    }, 10);
+      this.spinner = false
+    }, 0);
   }
 
-  ScrollIntoView(elem: string) {
-    document.querySelector(elem)!.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  Alert(type: string, text: string) {
+    this.alert = { type: type, text: text }
+    setTimeout(() => this.alert = {type: '', text: ''}, 3000);
+  }
+
+  ScrollInto(item: any) {
+    item.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
